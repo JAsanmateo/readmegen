99bbdb5ab8c5c292a4748e41ecbe394fe100f0f9
Jose San Mateo <info@joseasanmateo.com>
2022-09-04 16:56:32 -0700

    Finished Inquirer Prompts in index.js

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generatePage = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'project_title',
+          message: 'What is the title of your project?',
+        },
+        {
+          type: 'list',
+          name: 'license',
+          message: 'What kind of lisence does your project have?',
+          Choices: ["MIT", "GNU"],
+          default: ["MIT"]
+        },
+        {
+          type: 'input',
+          message: 'Please write the description for your project.',
+        //   name: //Template literal from Markdownfiles
+        },
+        {
+          type: 'input',
+          name: 'Installation',
+          message: 'Please write installation instructions for your project',
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub Username',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Please describe the usage for your project',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Please write some credits for your project',
+          },
+        {
+            type: 'input',
+            name: 'Questions?',
+            message: 'Provide an email address for questions to be directed too',
+          },
+      ])
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = data => {
+    fs.writeFile("./instructions/README.md", data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+questions()
+.then(answers => {
+    return generatePage(answers);
+})
+
+.then(data => {
+    return writeFile(data);
+});
 
 // Function call to initialize app
-init();
+// init();
